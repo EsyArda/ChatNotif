0dce2c84ac09061146651d28d9552cf6267e7801
Lilian Hiault <lilian.hiault@laposte.net>
2022-11-04 00:13:15 +0100

    Improved options: new fonts, colors only in debug mode, hide options unavailable

--- ChatNotif/Options.lua
+++ ChatNotif/Options.lua
@@ -1,27 +1,46 @@
 function OptionsControl()
-    local boxHeight = 25;
-    local boxWidth = 200;
-    local scrollBarHeight = 10;
-    local scrollBarWidth = 100;
-    local xOffset = 15;
+    -- Pixel values
+    local boxHeight = 20;
+    local boxWidth = 320;
+    local xOffset = 20;
+    local yMargin = 10;
 
+    -- Fonts
+    local headerFont = Turbine.UI.Lotro.Font.VerdanaBold16;
+    local corpsFont = Turbine.UI.Lotro.Font.Verdana16;
 
     Options = Turbine.UI.Control();
     Turbine.Plugin.GetOptionsPanel = function(self) return Options; end
 
-    Options:SetBackColor(Turbine.UI.Color.DarkSlateGray);
+    if SETTINGS.DEBUG then Options:SetBackColor(Turbine.UI.Color.DarkSlateGray) end
 
-    local ypos = 0;
+    local yPos = 0;
+
+    -- Lock position
+    local lockPosition = Turbine.UI.Lotro.CheckBox();
+    lockPosition:SetParent(Options);
+    lockPosition:SetSize(boxWidth - xOffset, boxHeight);
+    lockPosition:SetPosition(0, yPos);
+    lockPosition:SetFont(headerFont);
+    lockPosition:SetText("Lock window position");
+    if SETTINGS.DEBUG then lockPosition:SetBackColor(Turbine.UI.Color.DarkOrange) end
+    lockPosition:SetChecked(SETTINGS.POSITION_LOCKED);
+    lockPosition.CheckedChanged = function(sender, args)
+        MyNotifWindow:SetLock(lockPosition:IsChecked());
+    end
+    yPos = yPos + lockPosition:GetHeight() + yMargin;
+
     -- Channel choice
     local channelsLabel = Turbine.UI.Label();
     channelsLabel:SetParent(Options);
     channelsLabel:SetSize(boxWidth, boxHeight);
-    channelsLabel:SetBackColor(Turbine.UI.Color.DarkRed);
-    channelsLabel:SetText("Channels");
-    channelsLabel:SetPosition(0, ypos);
+    if SETTINGS.DEBUG then channelsLabel:SetBackColor(Turbine.UI.Color.DarkRed) end
+    channelsLabel:SetFont(headerFont);
+    channelsLabel:SetPosition(0, yPos);
+    channelsLabel:SetText("Select channels");
 
     -- Chat types
-    ypos = channelsLabel:GetTop() + channelsLabel:GetHeight();
+    yPos = channelsLabel:GetTop() + channelsLabel:GetHeight();
     local channelsCheckbox = {};
     for name, code in pairs(Turbine.ChatType) do
         if type(code) == "number" then
@@ -29,9 +48,10 @@ function OptionsControl()
             channelsCheckbox[name] = Turbine.UI.Lotro.CheckBox();
             channelsCheckbox[name]:SetParent(Options);
             channelsCheckbox[name]:SetSize(boxWidth - xOffset, boxHeight);
-            channelsCheckbox[name]:SetPosition(xOffset, ypos);
+            channelsCheckbox[name]:SetPosition(xOffset, yPos);
+            channelsCheckbox[name]:SetFont(corpsFont);
             channelsCheckbox[name]:SetText(name);
-            channelsCheckbox[name]:SetBackColor(Turbine.UI.Color.BlueViolet);
+            if SETTINGS.DEBUG then channelsCheckbox[name]:SetBackColor(Turbine.UI.Color.BlueViolet) end
             channelsCheckbox[name]:SetChecked(ExistsInSet(SETTINGS.CHANNELS_ENABLED, code));
             channelsCheckbox[name].CheckedChanged = function(sender, args)
                 if channelsCheckbox[name]:IsChecked() then
@@ -43,27 +63,32 @@ function OptionsControl()
                 end
             end
             -- Update Y position
-            ypos = ypos + boxHeight;
+            yPos = yPos + boxHeight;
         end
     end
+    yPos = yPos+ yMargin;
 
     -- Font size
     local fontSizeLabel = Turbine.UI.Label();
     fontSizeLabel:SetParent(Options);
     fontSizeLabel:SetSize(boxWidth, boxHeight);
-    fontSizeLabel:SetBackColor(Turbine.UI.Color.DarkGreen);
-    fontSizeLabel:SetPosition(0, ypos);
-    fontSizeLabel:SetText("Font size");
-    ypos = ypos + boxHeight;
+    if SETTINGS.DEBUG then fontSizeLabel:SetBackColor(Turbine.UI.Color.DarkGreen) end
+    fontSizeLabel:SetPosition(0, yPos);
+    fontSizeLabel:SetFont(headerFont);
+    fontSizeLabel:SetText("[In a future update] Select font size");
+    fontSizeLabel:SetVisible(false); -- TODO
+    yPos = yPos + boxHeight;
 
     -- Notif timer
     local timerLabel = Turbine.UI.Label();
     timerLabel:SetParent(Options);
     timerLabel:SetSize(boxWidth, boxHeight);
-    timerLabel:SetBackColor(Turbine.UI.Color.DarkGoldenrod);
-    timerLabel:SetPosition(0, ypos);
-    timerLabel:SetText("Notification timer");
-    
+    if SETTINGS.DEBUG then timerLabel:SetBackColor(Turbine.UI.Color.DarkGoldenrod) end
+    timerLabel:SetPosition(0, yPos);
+    timerLabel:SetFont(headerFont);
+    timerLabel:SetText("[In a future update] Notification duration");
+    timerLabel:SetVisible(false); -- TODO
+
     -- local timerMenu = Turbine.UI.ContextMenu();
     -- local timerMenuItems = timerMenu:GetItems();
     -- for i = 1, 10 do
@@ -73,74 +98,21 @@ function OptionsControl()
     --     local timerMenuItem = timerMenuItems:Get(i);
     --     local timerItemText = timerMenuItems:GetText();
     --     if ( timerItemText == self.selectedItem ) then
-	-- 		timerMenuItems:SetChecked( true );
-	-- 		timerMenuItems:SetEnabled( false );
-	-- 	end
+    -- 		timerMenuItems:SetChecked( true );
+    -- 		timerMenuItems:SetEnabled( false );
+    -- 	end
     -- end
 
     local timerButton = Turbine.UI.Lotro.Button();
     timerButton:SetParent(Options);
-    timerButton:SetPosition(boxWidth, ypos);
-    timerButton:SetWidth(boxWidth);
+    timerButton:SetPosition(boxWidth, yPos);
+    timerButton:SetWidth(100);
     timerButton:SetText("Set time");
+    timerButton:SetVisible(false); -- TODO
     timerButton.Click = function(sender, args)
         -- timerMenu:ShowMenu();
     end
+    yPos = yPos + boxHeight;
 
-    ypos = ypos + boxHeight;
-
-    -- Position
-    local positionLabel = Turbine.UI.Label();
-    positionLabel:SetParent(Options);
-    positionLabel:SetSize(boxWidth, boxHeight);
-    positionLabel:SetBackColor(Turbine.UI.Color.DarkBlue);
-    positionLabel:SetPosition(0, ypos);
-    positionLabel:SetText("Position");
-   
-    ypos = ypos + boxHeight;
-
-    -- Lock position
-    local lockPosition = Turbine.UI.Lotro.CheckBox();
-    lockPosition:SetParent(Options);
-    lockPosition:SetSize(boxWidth - xOffset, boxHeight);
-    lockPosition:SetPosition(xOffset, ypos);
-    lockPosition:SetText("Lock position");
-    lockPosition:SetBackColor(Turbine.UI.Color.DarkOrange);
-    lockPosition:SetChecked(SETTINGS.POSITION_LOCKED);
-    lockPosition.CheckedChanged = function(sender, args)
-        -- TODO
-        Turbine.Shell.WriteLine("Lock position: " .. tostring(lockPosition:IsChecked()));
-        MyNotifWindow:SetLock(lockPosition:IsChecked());
-    end
-
-    ypos = ypos + boxHeight;
-
-    -- TODO Position initiale des scrollbars
-    -- X ScrollBar
-    local XPositionScrollBar = Turbine.UI.Lotro.ScrollBar();
-    XPositionScrollBar:SetParent(Options);
-    XPositionScrollBar:SetPosition(scrollBarHeight, ypos);
-    XPositionScrollBar:SetSize(scrollBarWidth, scrollBarHeight);
-    XPositionScrollBar:SetOrientation(Turbine.UI.Orientation.Horizontal);
-    XPositionScrollBar.ValueChanged = function(sender, args)
-        local x = XPositionScrollBar:GetValue() * (Turbine.UI.Display:GetWidth() - MyNotifWindow:GetWidth()) / 100;
-        SETTINGS.POSITION.X = x;
-        MyNotifWindow:SetPosition(x, MyNotifWindow:GetTop());
-    end
-    ypos = ypos + scrollBarHeight;
-
-    -- Y ScrollBar
-    local YPositionScrollBar = Turbine.UI.Lotro.ScrollBar();
-    YPositionScrollBar:SetParent(Options);
-    YPositionScrollBar:SetPosition(0, ypos);
-    YPositionScrollBar:SetSize(scrollBarHeight, scrollBarWidth);
-    YPositionScrollBar:SetOrientation(Turbine.UI.Orientation.Vertical);
-    YPositionScrollBar.ValueChanged = function(sender, args)
-        local y = YPositionScrollBar:GetValue() * (Turbine.UI.Display:GetHeight() - MyNotifWindow:GetHeight()) / 100;
-        SETTINGS.POSITION.Y = y;
-        MyNotifWindow:SetPosition(MyNotifWindow:GetLeft(), y);
-    end
-    ypos = ypos + scrollBarWidth;
-
-    Options:SetSize(2 * boxWidth, ypos);
+    Options:SetSize(2 * boxWidth, yPos);
 end
