2d56146c04e54e561029209aa269f8a7e7bcea76
Lilian Hiault <lilian.hiault@laposte.net>
2023-02-02 11:35:55 +0100

    Added a Set class instead of functions

--- ChatNotif/Set.lua
+++ ChatNotif/Set.lua
@@ -5,7 +5,7 @@ end
 function AddToSet(set, element)
     if (set ~= nil and type(set) == "table" and element ~= nil) then
         set[element] = true;
-        print(tostring(element) .. " added");
+        -- print(tostring(element) .. " added");
     end
 end
 
@@ -19,6 +19,29 @@ end
 function RemoveFromSet(set, element)
     if (set ~= nil and type(set) == "table" and element ~= nil) then
         set[element] = false;
-        print(tostring(element) .. " removed");
+        -- print(tostring(element) .. " removed");
+    end
+end
+
+-- Simple Set class
+
+Set = { set = {} };
+
+-- Add an element to the set
+function Set:add(element)
+    if (set ~= nil and type(set) == "table" and element ~= nil) then
+        self.set[element] = true;
+    end
+end
+
+-- Check if an element is in the set
+function Set:exists(element)
+    return element ~= nil and self.set ~= nil and self.set[element];
+end
+
+-- Remove an element from the set
+function Set:remove(element)
+    if (set ~= nil and type(set) == "table" and element ~= nil) then
+        self.set[element] = false;
     end
 end
