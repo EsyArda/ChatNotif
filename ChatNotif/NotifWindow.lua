ba0bc74148d95d45eed69d7f7986327e15e05c04
Lilian Hiault <lilian.hiault@laposte.net>
2023-02-02 13:42:43 +0100

    Added more comments in NotifWindow

--- ChatNotif/NotifWindow.lua
+++ ChatNotif/NotifWindow.lua
@@ -1,35 +1,39 @@
+-- NotifWindow
+
 import "Esy.ChatNotif.Timer";
 import "Esy.ChatNotif.Callback";
 
--- Afficher une annonce
-NotifWindow = class(Turbine.UI.Window);
+-- NotifWindow class displays a window with a given message
+NotifWindow = class(Turbine.UI.Window); -- Inherit from Turbine.UI.Window
 
 function NotifWindow:Constructor()
-    -- Window
+    -- Turbin Window fields
     Turbine.UI.Window.Constructor(self);
     self:SetSize(500, 100);
     self:SetPosition(SETTINGS.POSITION.X, SETTINGS.POSITION.Y);
-    if SETTINGS.DEBUG then self:SetBackColor(Turbine.UI.Color(0.5, 0.18, 0.31, 0.31));
-    end
+    if SETTINGS.DEBUG then self:SetBackColor(Turbine.UI.Color(0.5, 0.18, 0.31, 0.31)) end
     self:SetWantsKeyEvents(false);
     self:SetLock(SETTINGS.POSITION_LOCKED);
     self:SetVisible(true);
 
-    -- Be able to move the window
+    -- Ability to move the window when unlocked
     local mousePositionBefore = nil;
     local windowPositionBefore = nil;
     local dragging = false;
+    -- On moouse click
     self.MouseDown = function(args)
         if SETTINGS.DEBUG then Turbine.Shell.WriteLine("MouseDown " .. tostring(args)) end
         mousePositionBefore = { Turbine.UI.Display.GetMousePosition(); }
         windowPositionBefore = { self:GetPosition(); }
         dragging = true;
     end
+    -- On mouse release
     self.MouseUp = function(args)
         if SETTINGS.DEBUG then Turbine.Shell.WriteLine("MouseUp " .. tostring(args)) end
         dragging = false;
         SETTINGS.POSITION.X, SETTINGS.POSITION.Y = self:GetPosition();
     end
+    -- On mouse move
     self.MouseMove = function(args)
         if (dragging) then
             local mouseBeforeX, mouseBeforeY = unpack(mousePositionBefore);
@@ -59,18 +63,19 @@ function NotifWindow:Constructor()
     self.Anounce:SetMouseVisible(false);
 
     -- Timer
-    -- Instantiate an instance of the timer
+    -- Instantiate the timer
     DisplayTimer = Timer();
-    -- Set the TimeReached event handler
+    -- Default blank message
     local clearMsg = function()
         self:DisplayMsg("");
     end
+    -- Set the TimeReached event handler to clear the message when the timer ends
     AddCallback(DisplayTimer, "TimeReached", clearMsg);
-
     -- On message received
     self.ChatReceived(self);
 end
 
+-- Lock or unlock the window position
 function NotifWindow:SetLock(lockState)
     if (not lockState) then
         self:SetBackColor(Turbine.UI.Color(0.5, 0.18, 0.31, 0.31));
@@ -83,20 +88,21 @@ function NotifWindow:SetLock(lockState)
     SETTINGS.POSITION_LOCKED = lockState
 end
 
+-- Displays a message for a given duration
 function NotifWindow:DisplayMsg(msg, duration)
     if (duration ~= 0) then DisplayTimer:SetTime(duration, false) end
     self.Anounce:SetText(msg);
     self.Anounce:SetVisible(true);
-    --timer puis setvisible(false);
 end
 
+-- Check if the message should be displayed given the current settings
 function NotifWindow:ShouldDisplay(message)
-    -- Check if UI is hidden
+    -- TODO Check if UI is hidden
     return SETTINGS.CHANNELS_ENABLED[message.ChatType];
 end
 
+-- On message received
 function NotifWindow:ChatReceived()
-    --- Lire les messages reçus
     Turbine.Chat.Received = function(sender, args)
         if (self:ShouldDisplay(args)) then
             local msg;
