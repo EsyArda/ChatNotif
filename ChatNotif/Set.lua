0d1778e60a3a7d5e9942f68f8bce00522118574d
Lilian Hiault <lilian.hiault@laposte.net>
2023-03-31 18:29:06 +0200

    Removed Set class

--- ChatNotif/Set.lua
+++ ChatNotif/Set.lua
@@ -20,26 +20,3 @@ function RemoveFromSet(set, element)
         set[element] = false;
     end
 end
-
--- Simple Set class
-
-Set = { set = {} };
-
--- Add an element to the set
-function Set:add(element)
-    if (set ~= nil and type(set) == "table" and element ~= nil) then
-        self.set[element] = true;
-    end
-end
-
--- Check if an element is in the set
-function Set:exists(element)
-    return element ~= nil and self.set ~= nil and self.set[element];
-end
-
--- Remove an element from the set
-function Set:remove(element)
-    if (set ~= nil and type(set) == "table" and element ~= nil) then
-        self.set[element] = false;
-    end
-end
