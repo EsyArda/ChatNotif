643deddce244dfd2fdecf845f5dc55d812e12101
Lilian Hiault <lilian.hiault@laposte.net>
2023-05-29 20:10:26 +0200

    Renamed logo to icon

--- create-release-windows.ps1
+++ create-release-windows.ps1
@@ -5,7 +5,7 @@ Copy-Item .\ChatNotif.plugincompendium .\tmp\Esy\
 New-Item -Type Directory .\tmp\Esy\ChatNotif > $null
 Copy-Item .\ChatNotif\*.lua .\tmp\Esy\ChatNotif\
 New-Item -Type Directory .\tmp\Esy\ChatNotif\res > $null
-Copy-Item .\ChatNotif\res\ring.tga .\tmp\Esy\ChatNotif\res\
+Copy-Item .\ChatNotif\res\icon.tga .\tmp\Esy\ChatNotif\res\
 Write-Output "See https://github.com/LilianHiault/ChatNotif/blob/main/README.md for more info." > .\tmp\Esy\ChatNotif\README.txt
 Copy-Item .\LICENSE.md .\tmp\Esy\ChatNotif
 # Git tag
