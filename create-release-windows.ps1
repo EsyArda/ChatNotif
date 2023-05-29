fd2f144e85b57288e7f85cf45bc92eb30a6f4165
Lilian Hiault <lilian.hiault@laposte.net>
2023-05-29 18:05:49 +0200

    License included in the release zip

--- create-release-windows.ps1
+++ create-release-windows.ps1
@@ -7,5 +7,6 @@ Copy-Item .\ChatNotif\*.lua .\tmp\Esy\ChatNotif\
 New-Item -Type Directory .\tmp\Esy\ChatNotif\res > $null
 Copy-Item .\ChatNotif\res\ring.tga .\tmp\Esy\ChatNotif\res\
 Write-Output "See https://github.com/LilianHiault/ChatNotif/blob/main/README.md for more info." > .\tmp\Esy\ChatNotif\README.txt
-Compress-Archive -LiteralPath .\tmp\Esy\ -DestinationPath release.zip
+Copy-Item .\LICENSE.md .\tmp\Esy\ChatNotif
+Compress-Archive -LiteralPath .\tmp\Esy\ -DestinationPath Esy-ChatNotif-X.Y.Z.zip
 Remove-Item -Recurse .\tmp\
