a272c7c61f6dac75c05d256befbf56bc7158527c
Lilian Hiault <lilian.hiault@laposte.net>
2024-11-27 23:20:38 +0100

    Changed util to get table content

--- ChatNotif/Utils.lua
+++ ChatNotif/Utils.lua
@@ -1,13 +1,16 @@
-function RecursivePrintTable(k, v)
-    if type(v) == "table" then
-        for key, value in pairs(v) do
-            Turbine.Shell.WriteLine(tostring(k)..":")
-            RecursivePrintTable(key, value)
-        end
+function DumpTable(table)
+    -- https://stackoverflow.com/questions/9168058/how-to-dump-a-table-to-console
+    if type(table) == 'table' then
+       local string = '{ '
+       for key,value in pairs(table) do
+          if type(key) ~= 'number' then key = '"'..key..'"' end
+          string = string .. '['..key..'] = ' .. DumpTable(value) .. ','
+       end
+       return string .. '} '
     else
-        Turbine.Shell.WriteLine("["..tostring(k).."] = "..tostring(v))
+       return tostring(table)
     end
-end
+ end
 
 function Clamp(value, min, max)
     return math.max(math.min(value, max), min)
